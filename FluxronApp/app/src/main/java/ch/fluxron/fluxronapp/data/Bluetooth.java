610e30d49cec09f31311bd5b219fb4a1679ffa2b
TheoWinter <theo.winter@gmail.com>
2015-10-09 11:00:04 +0200

    Connecting to a specific bt device.

--- FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/data/Bluetooth.java
+++ FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/data/Bluetooth.java
@@ -1,7 +1,12 @@
 package ch.fluxron.fluxronapp.data;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 import ch.fluxron.fluxronapp.events.modelDal.BluetoothDiscoveryRequest;
 
@@ -17,9 +22,28 @@ public class Bluetooth {
     }
 
     public void onEventAsync(BluetoothDiscoveryRequest msg) {
-        Log.d("FLUXRON","Received BT Message");
-        /*
         BluetoothAdapter btAdapter = setupBluetooth();
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        List<String> s = new ArrayList<String>();
+
+        BluetoothDevice flux = null;
+        if (pairedDevices != null) {
+            for(BluetoothDevice device : pairedDevices){
+                Log.d("FLUXRON", device.getName()+" "+device.getAddress());
+                //Example: FLX_GTZ_196 00:13:04:12:06:20
+                s.add(device.getName());
+                if(device.getAddress().equals("00:13:04:12:06:20")){
+                    flux = device;
+                    Log.d("FLUXRON", "DEVICE FOUND");
+                    break;
+                }
+            }
+
+        } else {
+            //TODO: Handle case when there are no paired devices
+            Log.d("FLUXRON", "NO PAIRED DEVICES FOUND");
+        }
+        /*
         Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
         List<String> s = new ArrayList<String>();
         for(BluetoothDevice device : pairedDevices){
