33101c205e92f81f3e05c846fb68a3afc6438a0a
TheoWinter <theo.winter@gmail.com>
2015-10-09 11:51:51 +0200

    Connecting to specific bt device and establishing a socket for communication.

--- FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/data/Bluetooth.java
+++ FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/data/Bluetooth.java
@@ -2,11 +2,17 @@ package ch.fluxron.fluxronapp.data;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Build;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.UUID;
 
 import ch.fluxron.fluxronapp.events.modelDal.BluetoothDiscoveryRequest;
 
@@ -15,6 +21,17 @@ import ch.fluxron.fluxronapp.events.modelDal.BluetoothDiscoveryRequest;
  */
 public class Bluetooth {
     private IEventBusProvider provider;
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private OutputStream outStream = null;
+
+    private static final String TAG = "FLUXRON";
+
+    // SPP UUID service
+    private static final UUID MY_UUID = UUID.fromString("13371337-0000-9999-8000-00805F9B34FB");
+
+    //Bluetooth Device MAC
+    private static String address = "00:13:04:12:06:20";
 
     public Bluetooth(IEventBusProvider provider) {
         this.provider = provider;
@@ -22,36 +39,68 @@ public class Bluetooth {
     }
 
     public void onEventAsync(BluetoothDiscoveryRequest msg) {
-        BluetoothAdapter btAdapter = setupBluetooth();
+        btAdapter = setupBluetooth();
         Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
         List<String> s = new ArrayList<String>();
 
         BluetoothDevice flux = null;
         if (pairedDevices != null) {
             for(BluetoothDevice device : pairedDevices){
-                Log.d("FLUXRON", device.getName()+" "+device.getAddress());
+                Log.d(TAG, device.getName()+" "+device.getAddress());
                 //Example: FLX_GTZ_196 00:13:04:12:06:20
                 s.add(device.getName());
-                if(device.getAddress().equals("00:13:04:12:06:20")){
+                if(device.getAddress().equals(address)){
                     flux = device;
-                    Log.d("FLUXRON", "DEVICE FOUND");
+                    Log.d(TAG, "Device found");
                     break;
                 }
             }
-
         } else {
             //TODO: Handle case when there are no paired devices
-            Log.d("FLUXRON", "NO PAIRED DEVICES FOUND");
+            Log.d(TAG, "No paird devices found");
         }
-        /*
-        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
-        List<String> s = new ArrayList<String>();
-        for(BluetoothDevice device : pairedDevices){
-            s.add(device.getName());
-            BluetoothDiscoveryResponse response = new BluetoothDiscoveryResponse(device.getName(), device.getAddress());
-            provider.getDalEventBus().post(response);
+
+        //Connection
+        if(flux != null){
+            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+            try {
+                btSocket = createBluetoothSocket(device);
+            } catch (IOException e1) {
+                Log.d("Fatal Error", "In onResume() and socket create failed: " + e1.getMessage() + ".");
+            }
+            btAdapter.cancelDiscovery();
+
+            Log.d(TAG, "Connecting");
+            try {
+                btSocket.connect();
+                Log.d(TAG, "Connection ok");
+            } catch (IOException e) {
+                try {
+                    btSocket.close();
+                } catch (IOException e2) {
+                    Log.d(TAG, e2.getMessage());
+                }
+
+                Log.d(TAG, "Create Socket");
+                try {
+                    outStream = btSocket.getOutputStream();
+                } catch (IOException ed) {
+                    Log.d(TAG, ed.getMessage());
+                }
+            }
         }
-        btAdapter.startDiscovery();*/
+    }
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        if(Build.VERSION.SDK_INT >= 10){
+            try {
+                final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", new Class[] { UUID.class });
+                return (BluetoothSocket) m.invoke(device, MY_UUID);
+            } catch (Exception e) {
+                Log.e(TAG, "Could not create Insecure RFComm Connection", e);
+            }
+        }
+        return  device.createRfcommSocketToServiceRecord(MY_UUID);
     }
 
     private BluetoothAdapter setupBluetooth(){
@@ -61,14 +110,14 @@ public class Bluetooth {
             // Continue with bluetooth setup.
         } else {
             //TODO: Handle error, no Bluetooth Adapter
-            Log.d("FLUXRON", "NO BLUETOOTH ADAPTER");
+            Log.d(TAG, "No Bluetooth adapter");
         }
 
         if (btAdapter.isEnabled()) {
             // Enabled. Work with Bluetooth.
         } else {
             //TODO: Offer user to enable Bluetooth.
-            Log.d("FLUXRON", "BLUETOOTH NOT ENABLED");
+            Log.d(TAG, "Bluetooth not enabled");
         }
         return btAdapter;
     }
