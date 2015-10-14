1f0ece146a207c7a9abc2506488dc42e8fe39793
TheoWinter <theo.winter@gmail.com>
2015-10-14 11:50:53 +0200

    Attempt at reading .od files containing param infos.

---
+++ FluxronApp/app/src/androidTest/java/ch/fluxron/fluxronapp/ParamImporterTest.java
@@ -0,0 +1,15 @@
+package ch.fluxron.fluxronapp;
+import android.test.AndroidTestCase;
+
+import ch.fluxron.fluxronapp.data.ParamImporter;
+
+/**
+ * Unit-test for ParamImporter class.
+ */
+public class ParamImporterTest extends AndroidTestCase {
+
+    public void testSomething(){
+        ParamImporter paramImporter = new ParamImporter(getContext());
+        paramImporter.loadOD();
+    }
+}
