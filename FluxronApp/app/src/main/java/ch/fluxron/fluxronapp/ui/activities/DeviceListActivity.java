12b1e05dd6b1d16a8400cb3d8a49487532e4cc88
TheoWinter <theo.winter@gmail.com>
2015-10-29 11:13:18 +0100

    added IDeviceClickListener

--- FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/ui/activities/DeviceListActivity.java
+++ FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/ui/activities/DeviceListActivity.java
@@ -2,17 +2,20 @@ package ch.fluxron.fluxronapp.ui.activities;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import ch.fluxron.fluxronapp.R;
+import ch.fluxron.fluxronapp.model.Device;
 import ch.fluxron.fluxronapp.ui.activities.common.FluxronBaseActivity;
+import ch.fluxron.fluxronapp.ui.adapters.IDeviceClickListener;
 import ch.fluxron.fluxronapp.ui.fragments.DeviceListFragment;
 
 /**
  * Temporary activity, created to work on the deviceList fragment. Can be removed once
  * the fragment is working and added to its correct activity.
  */
-public class DeviceListActivity extends FluxronBaseActivity {
+public class DeviceListActivity extends FluxronBaseActivity implements IDeviceClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,7 @@ public class DeviceListActivity extends FluxronBaseActivity {
         // Initialize the area fragment
         DeviceListFragment fragment = new DeviceListFragment();
         fragment.setEventBusProvider(this.busProvider);
+        fragment.setClickListener(this);
 
         // Fade in the fragment for area
         FragmentTransaction ft = getFragmentManager().beginTransaction();
@@ -42,4 +46,8 @@ public class DeviceListActivity extends FluxronBaseActivity {
         finish();
     }
 
+    @Override
+    public void deviceClicked(Device d) {
+        Log.d("Fluxron", "CLICKED!!");
+    }
 }
--- FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/ui/fragments/DeviceListFragment.java
+++ FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/ui/fragments/DeviceListFragment.java
@@ -3,13 +3,17 @@ package ch.fluxron.fluxronapp.ui.fragments;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
 import ch.fluxron.fluxronapp.R;
+import ch.fluxron.fluxronapp.ui.adapters.DeviceListAdapter;
+import ch.fluxron.fluxronapp.ui.adapters.IAreaClickedListener;
+import ch.fluxron.fluxronapp.ui.adapters.IDeviceClickListener;
+import ch.fluxron.fluxronapp.ui.decorators.SpacesItemDecoration;
 import ch.fluxron.fluxronapp.ui.util.IEventBusProvider;
 
 /**
@@ -17,6 +21,8 @@ import ch.fluxron.fluxronapp.ui.util.IEventBusProvider;
  */
 public class DeviceListFragment extends Fragment {
     private IEventBusProvider provider;
+    private DeviceListAdapter listAdapter;
+    private IDeviceClickListener listener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,10 @@ public class DeviceListFragment extends Fragment {
 
     public void setEventBusProvider(IEventBusProvider provider) {
         this.provider = provider;
+    }
+
+    public void setClickListener(IDeviceClickListener listener){
+        this.listener = listener;
     }
 
     @Override
@@ -49,12 +59,25 @@ public class DeviceListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View listView = getActivity().getLayoutInflater().inflate(R.layout.fragment_device_list, container, false);
-        ScrollView scroller = new ScrollView(getActivity());
-        TextView text = new TextView(getActivity());
-        scroller.addView(text);
-        text.setText("Activity Test text");
-        return scroller;
+        View deviceView = getActivity().getLayoutInflater().inflate(R.layout.fragment_device_list, container, false);
+
+        // Set the list's properties
+        RecyclerView deviceListView = (RecyclerView) deviceView.findViewById(R.id.deviceList);
+
+        // Layout for the list
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        deviceListView.setLayoutManager(layoutManager);
+
+        // Item decoration for the list
+        SpacesItemDecoration deco = new SpacesItemDecoration(30);
+        deviceListView.addItemDecoration(deco);
+
+        // List adapter
+        listAdapter = new DeviceListAdapter(this.listener, this.provider);
+        deviceListView.setAdapter(listAdapter);
+
+        return deviceView;
     }
 
     public void onEventMainThread(Object msg){
