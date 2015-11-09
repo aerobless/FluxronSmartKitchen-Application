0fab4c03c0e7ad01c48d3bae0388dc128526ffca
TheoWinter <theo.winter@gmail.com>
2015-11-09 11:32:29 +0100

    setting the display text of the paramView either from the manager or manually.

--- FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/ui/components/ParameterView.java
+++ FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/ui/components/ParameterView.java
@@ -6,19 +6,42 @@ import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import ch.fluxron.fluxronapp.R;
+import ch.fluxron.fluxronapp.data.generated.ParamManager;
 
 public class ParameterView extends RelativeLayout {
+    TypedArray arguments;
+    ParamManager manager;
+    String parameter;
+    TextView paramName;
+    TextView paramValue;
 
     public ParameterView(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.component_parameter_view, this, true);
+        manager = new ParamManager();
 
-        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.ParameterView);
-        String myText = a.getString(R.styleable.ParameterView_paramName);
-        
+        arguments = context.obtainStyledAttributes(attrs, R.styleable.ParameterView);
+        parameter = arguments.getString(R.styleable.ParameterView_paramName);
 
+        paramName = (TextView) this.findViewById(R.id.paramName);
+        paramValue = (TextView) this.findViewById(R.id.paramValue);
+
+        setDisplayText();
+    }
+
+    /**
+     * If no displayText is supplied the default param Name is used.
+     */
+    private void setDisplayText() {
+        String displayText = arguments.getString(R.styleable.ParameterView_paramDisplayText);
+        if(displayText != null){
+            paramName.setText(displayText);
+        } else {
+            paramName.setText(manager.getParamMap().get(parameter).getName());
+        }
     }
 
     public ParameterView(Context context, AttributeSet attrs, int defStyleAttr) {
