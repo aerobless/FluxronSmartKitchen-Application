ab5cdda2794f6e0b0a0282236a48a77aff42cae6
unknown <kkayed@PIN1258013.hsr.ch>
2015-11-18 13:06:16 +0100

    Manifest and kitchen object are now in the export file.

---
+++ FluxronApp/app/src/main/java/ch/fluxron/fluxronapp/model/FluxronManifest.java
@@ -0,0 +1,54 @@
+package ch.fluxron.fluxronapp.model;
+
+import java.util.Date;
+
+/**
+ * Represents the manifest object packed into .fluxron files
+ */
+public class FluxronManifest {
+    private String objectType;
+    private String objectName;
+    private String objectDescription;
+    private String objectId;
+    private Date   saveDate;
+
+    public String getObjectType() {
+        return objectType;
+    }
+
+    public void setObjectType(String objectType) {
+        this.objectType = objectType;
+    }
+
+    public String getObjectName() {
+        return objectName;
+    }
+
+    public void setObjectName(String objectName) {
+        this.objectName = objectName;
+    }
+
+    public String getObjectDescription() {
+        return objectDescription;
+    }
+
+    public void setObjectDescription(String objectDescription) {
+        this.objectDescription = objectDescription;
+    }
+
+    public Date getSaveDate() {
+        return saveDate;
+    }
+
+    public void setSaveDate(Date saveDate) {
+        this.saveDate = saveDate;
+    }
+
+    public String getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+        this.objectId = objectId;
+    }
+}
